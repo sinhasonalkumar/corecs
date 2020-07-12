43b178fe4da7825ae83d90b222b92ba9751634aa
sonal <sinha.sonal.kumar@gmail.com>
2020-07-12 14:24:36 -0700

    LargestKthElementInStream

---
+++ src/main/java/corecs/sorting/LargestKthElementInStream.java
@@ -0,0 +1,86 @@
+package corecs.sorting;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LargestKthElementInStream {
+
+	/*
+	 Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order,
+	 not the kth distinct element.
+	 Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, 
+	 which contains initial elements from the stream. For each call to the method KthLargest.add, 
+	 return the element representing the kth largest element in the stream.
+	 
+	 	int k = 3;
+		int[] arr = [4,5,8,2];
+		KthLargest kthLargest = new KthLargest(3, arr);
+		kthLargest.add(3);   // returns 4
+		kthLargest.add(5);   // returns 5
+		kthLargest.add(10);  // returns 5
+		kthLargest.add(9);   // returns 8
+		kthLargest.add(4);   // returns 8
+	 
+	 */
+	
+	public static void main(String[] args) {
+		
+		int k = 3;
+		int[] arr = {4,5,8,2};
+		KthLargest kthLargest = new KthLargest(k, arr);
+		
+		int kthLargestElement;
+		
+		kthLargestElement = kthLargest.add(3);   // returns 4
+		
+		System.out.println(kthLargestElement);
+		
+		kthLargestElement = kthLargest.add(5);   // returns 5
+		
+		System.out.println(kthLargestElement);
+		
+		kthLargestElement = kthLargest.add(10);  // returns 5
+		
+		System.out.println(kthLargestElement);
+		
+		kthLargestElement = kthLargest.add(9);   // returns 8
+		
+		System.out.println(kthLargestElement);
+		
+		kthLargestElement = kthLargest.add(4);   // returns 8
+		
+		System.out.println(kthLargestElement);
+		
+	}
+}
+
+class KthLargest {
+
+	//PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
+	private Queue<Integer> minHeap;
+
+	KthLargest(int k, int[] arr) {
+
+		 this.minHeap = new PriorityQueue<Integer>(k);
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (this.minHeap.size() < k) {
+				this.minHeap.add(arr[i]);
+			} else {
+				add(arr[i]);
+			}
+		}
+	}
+	
+	int add(int n) {
+		
+		if (n > this.minHeap.peek()) {
+			this.minHeap.remove();
+			this.minHeap.add(n);
+		}
+		
+		return this.minHeap.peek();
+		
+	}
+}
